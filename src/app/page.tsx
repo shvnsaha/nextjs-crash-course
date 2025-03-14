b8a567d62bd7355d1f8922b970a6f01702b5767e
shvnsaha <shovan.css2018@gmail.com>
2025-03-15 00:22:39 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-yellow-400">
+      Next js Full Course 2025
+    </div>
+  );
+}
---
+++ src/app/performance/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PerformancePage = () => {
+    return (
+        <div>
+            Performance Page
+        </div>
+    );
+};
+
+export default PerformancePage;
